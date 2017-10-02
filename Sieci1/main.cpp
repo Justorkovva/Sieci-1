bfef37b0c9b1f974d5653faf32393b51921bb12e
Justyna Mackoś <j.mackos@wp.pl>
2017-10-02 16:42:12 +0200

    First commit, errors everywhere :)

---
+++ Sieci1/main.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include<fstream>
+#include<string>
+using namespace std;
+/* void zapisz(string text) {
+ // nie jest dokonczona i nwm czy jest sens jej u¿ywaæ
+    ofstream plik("bin.bin", ios::binary);
+    plik.write(reinterpret_cast<char*>(&text), sizeof(string));
+} */
+
+int main()
+{
+     //wczytuje plik. co robi ifstream::binary, albo ios::binary.  ?
+
+    ifstream infile( "doc.txt", ios::binary );
+
+// Copy a file
+  ofstream outfile ("new.txt", ios::binary);
+
+  // get size of file
+  infile.seekg (0,infile.end);
+  long size = infile.tellg();
+  infile.seekg (0);
+
+  // allocate memory for file content
+  char* buffer = new char[size];
+
+  // read content of infile
+  infile.read (buffer,size);
+
+  // write to outfile
+  outfile.write (buffer,size);
+
+  // release dynamically-allocated memory
+  delete[] buffer;
+
+  outfile.close();
+  infile.close();
+  return 0;
+   // moge to zrobic na stringach i pojdzie dosc latwo, albo bawic sie w tablice bitow gdzie jest trudniej :/
+
+
+    //zamienic na binarny   buffor bajtów?  tablica bajtów
+
+    //jakos go przechowac najlepiej nie w stringu, aczkolwiek nie wiem czemu.
+    //moze klasa biginteger w javie? to tez string :/
+
+    //jak juz jest wczytany to 3 algorytmy na niego napisac i wyliczyc wartoœci
+
+    //dodac bledy ktorych wartosc mozna zmienic np. 0,1% , 0,01 % . sufit z tej wartosci
+
+    //wyliczyc jeszcze raz wartosci
+
+    //porownac i zapisac recznie w excelu
+
+    //size of , wskaŸniki , diff :(
+
+    // zeby sprawdzic jaki bit to dodac np. 1010 and 0001 jak jest rozne od 0 to na ostanim bylo 1
+
+    //wszystkie pliki sa binarne normalnie wiec nauczyc sie je czytac
+
+    //w krypto 4 zad 2 nwm czy dziala mam zamiane bitow na stringi
+
+    cout << "Hello world!" << endl;
+    return 0;
+}

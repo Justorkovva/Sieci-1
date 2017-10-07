777d54bb06c490a2b1ae37656b6a36e4f447ff21
Justyna Mackoś <j.mackos@wp.pl>
2017-10-07 13:21:10 +0200

    CRC in progress

--- Sieci1/main.cpp
+++ Sieci1/main.cpp
@@ -15,9 +15,10 @@ using namespace std;
 */
 int main()
 {
-    int i,j,int_char,length,bit_total_1=0, bit_mod2_1,algorytm=0,m,bit_mod;
+    int i,j,int_char,length,bit_total_1=0, bit_mod2_1,algorytm=1,m,bit_mod,n;
     bool bit_p1;
-    string byte;
+    string crc, crc2;
+
 
     //wczytuje plik. Czy jest różnica między ifstream::binary, a ios::binary? czy to w ogóle potrzebne?
     ifstream infile( "1.txt", ios::binary );
@@ -29,12 +30,15 @@ int main()
   long size = infile.tellg();
   infile.seekg (0);
 
+   // czytam z pierwszego pliku
   char* buffer = new char[size+1];
+   infile.read (buffer,size);
 
-  // czytam z pierwszego pliku
-  infile.read (buffer,size);
+  while(algorytm>0) {
 
-  cout<<" Ktory algorytm chcesz wybrac? \n 1 - bit parzystosci \n 2 - suma modulo \n 3 - Cykliczny kod nadmiarowy "<<endl;
+  buffer[size+1]=0; //zeruje sume kontrolna
+
+  cout<<" Ktory algorytm chcesz wybrac? \n 1 - bit parzystosci \n 2 - suma modulo \n 3 - Cykliczny kod nadmiarowy  \n 0 - Wyjscie"<<endl;
 cin>>algorytm;
 
   switch(algorytm) {
@@ -42,11 +46,9 @@ cin>>algorytm;
   case 1:
 //Licze bit parzystości
 for(i=0;i<size;i++)
-{
-    for(j=0;j<8;j++)
-    {
-    if((buffer[i] >> j) & 1)
-        bit_total_1++;
+{    for(j=0;j<8;j++) {
+        if((buffer[i] >> j) & 1)
+            bit_total_1++;
     }
 }
 bit_mod2_1=bit_total_1 % 2;
@@ -56,10 +58,14 @@ cout<<"Bit parzystosci dla oryginalu wynosi : "<<bit_mod2_1<<endl;
 
 buffer[size+1]=char(bit_mod2_1);
 
-/* buffer[size+1] |=bit_mod2_1<<0; //nie jestem pewna tego zapisu
+/* buffer[size+1] |=bit_mod2_1<<0; //nie jestem pewna tego zapisu, i tak zajmie 1 bajt w charze, to tak będzie łatwiej
 buffer[size+1] ^= (-bit_mod2_1 ^ buffer[size+1]) & (1 << 0);
-cout<<((buffer[i] >> 0) & 1)<<endl;
+cout<<((buffer[size+1] >> 0) & 1)<<endl;
 */
+
+//Dodaj zakłocenia - sufit żeby zawsze jakieś było.
+
+//Policz jeszcze raz
     break;
 
   case 2:
@@ -72,29 +78,63 @@ cout<<((buffer[i] >> 0) & 1)<<endl;
     cout<<"Podaj modulo "<<endl;
     cin>>m;
 bit_mod=bit_total_1 % m;
-cout<<"Bit parzystosci dla oryginalu wynosi : "<<bit_mod<<endl;
+cout<<"Suma modulo dla oryginału wynosi : "<<bit_mod<<endl;
 
-//Dodaje sume kontrolna, przechowuje ja w int
-
+//Dodaje sume kontrolna,
 buffer[size+1]=char(bit_mod);
-cout<<int(buffer[size+1]); //zwraca 1 lub 0
+//cout<<int(buffer[size+1]); //zwraca 1 lub 0
 
+//Dodaj zakłocenia - sufit żeby zawsze jakieś było.
+
+//Policz jeszcze raz
     break;
 
   case 3:
+     {/*
+      cout<<"Ilo bitowy kod CRC chcesz otrzymac?"<<endl;
+      cin>>n;
+      cout<<"Podaj CRC (o jeden wiecej bit niz podales przed chwila)"<<endl;
+      cin>>crc;
+      */}
+
+      n=3;
+     crc="1010";
+
+     for(i=0;i<(size);i++)
+     {  for(j=0;j<8;j++)
+         { //wsadzam bity do stringa
+             if((buffer[i] >> j) & 1)
+             crc2[j]='1';
+             else
+            crc2[j]='0';
+             cout<<crc2[j]<<endl;
+         }
+        // buffer[i]; na bity
+         //dzielenie xor
+     }
+
+
+
+
+    break;
+  case 0:
+    cout<<"Do widzenia"<<endl;
     break;
 
   default:
       cout<<"Wybrales zla wartosc"<<endl;
     break;
   }
+  }
+    // Zapisuję drugi plik (będzie można zaobserwować czy to taki sam/prawie taki sam)
+  outfile.write (buffer,size);
 
-
-
-
-
-
-//dodaje zaklocenia
+//żeby nie tworzyć lagów na komputerze
+  delete[] buffer;
+  outfile.close();
+  infile.close();
+  return 0;
+}
 
 
 /*
@@ -120,52 +160,9 @@ bit_total_1=bit_total_1 % 2;
 */
 //cout<<endl<<bit_total_1;
 
-
-  //obliczam dla pliku pierwszego
-
-  //dodaje sume kontrolna na koniec pliku
-
-  //dodaje zakłócenia do buffora
-
-  //usuwam sumę kontrolną?
-
-  //obliczam dla pliku drugiego
-
-  //porównuję
-
-  // Zapisuję drugi plik (będzie można zaobserwować czy to taki sam/prawie taki sam)
-  outfile.write (buffer,size);
-
-  //żeby nie tworzyć lagów na komputerze
-  delete[] buffer;
-  outfile.close();
-  infile.close();
-  return 0;
-
-
-   // moge to zrobic na stringach i pojdzie dosc latwo, albo bawic sie w tablice bitow gdzie jest trudniej :/
-
-    //zamienic na binarny   buffor bajtów?  tablica bajtów
-
-    //jakos go przechowac najlepiej nie w stringu, aczkolwiek nie wiem czemu.
-    //moze klasa biginteger w javie? to tez string :/
-
-    //jak juz jest wczytany to 3 algorytmy na niego napisac i wyliczyc wartoœci
-
-    //dodac bledy ktorych wartosc mozna zmienic np. 0,1% , 0,01 % . sufit z tej wartosci
-
-    //wyliczyc jeszcze raz wartosci
-
     //porownac i zapisac recznie w excelu
-
-    //size of , wskaŸniki , diff :(
-
+    //size of , wskazniki , diff :(
     // zeby sprawdzic jaki bit to dodac np. 1010 and 0001 jak jest rozne od 0 to na ostanim bylo 1
-
-    //wszystkie pliki sa binarne normalnie wiec nauczyc sie je czytac
-
     //w krypto 4 zad 2 nwm czy dziala mam zamiane bitow na stringi
 
-    cout << "Hello world!" << endl;
-    return 0;
-}
+
